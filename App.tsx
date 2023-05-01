48d9b3c76bcfef5987b47f3ce92e18d99524b364
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2023-05-01 22:54:01 +0800

    added fonts asset

--- App.tsx
+++ App.tsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { View, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
 import { AppNavigation } from "./src/navigation/AppNavigation";
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    "Lato-Thin": require("./assets/fonts/Lato-Thin.ttf"),
+    "Lato-ThinItalic": require("./assets/fonts/Lato-ThinItalic.ttf"),
+    "Lato-Light": require("./assets/fonts/Lato-Light.ttf"),
+    "Lato-LightItalic": require("./assets/fonts/Lato-LightItalic.ttf"),
+    "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
+    "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+    "Lato-BoldItalic": require("./assets/fonts/Lato-BoldItalic.ttf"),
+  });
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return <Text>Failed to load app assets</Text>;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="dark" />
-      <AppNavigation />
+      <StatusBar style="auto" />
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <AppNavigation />
+      </View>
     </SafeAreaView>
   );
 };
---
+++ src/libs/secure-http.lib.ts
@@ -0,0 +1,7 @@
+export const encryptHttpRequest = () => {
+  //
+};
+
+export const decryptHttpResponse = () => {
+  //
+};
